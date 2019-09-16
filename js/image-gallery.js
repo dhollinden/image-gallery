98c04bf52667a3bb5faf17c88697bbf225c5196c
Dave Hollinden <Dave.Hollinden@ithaka.org>
2019-09-16 06:14:57 -0400

    Add imgError function to hide images that return 404 status.

--- js/image-gallery.js
+++ js/image-gallery.js
@@ -1,48 +1,47 @@
 $(document).ready(loadPhotoData)
 
-let photoDataArray = []
+let photoArray = []
 let filteredPhotos = []
 let searchTerm = ""
 let photoHTML = ""
 
 function loadPhotoData() {
     $.getJSON("data.json", function (data) {
-        photoDataArray = data.photos.photo
-        filterAndDisplayPhotos(photoDataArray)
+        photoArray = data.photos.photo
+        filterAndDisplay(photoArray)
     })
 }
 
-function filterAndDisplayPhotos(array) {
-    filteredPhotos = filterPhotos(array)
+function filterAndDisplay(array) {
+    filteredPhotos = filterBySearchTerm(array)
     displayPhotos(filteredPhotos)
 }
 
-function filterPhotos(array) {
-    console.log("filterPhotos: invoked")
-    return array.filter(searchFilter)
-}
-
-function searchFilter(photo) {
-    console.log("searchFilter: invoked")
-    if (searchTerm) {
-        return photo.title.toLowerCase().includes(searchTerm.toLowerCase())
-    }
-    return true
+function filterBySearchTerm(array) {
+    return array.filter(function (photo) {
+        if (searchTerm) return photo.title.toLowerCase().includes(searchTerm.toLowerCase())
+        return true
+    })
 }
 
 function displayPhotos(array) {
-    console.log("displayPhotos: invoked")
     photoHTML = ""
     array.forEach(function(photo) {
-        photoHTML += '<div class="picbox"><figure><img src="' + photo.url_sq_cdn + '" class="frame" onclick="document.location=this.src"><figcaption>' + photo.title + '</figcaption></figure></div>'
+        // photoHTML += '<div class="picbox"><figure><img src="' + photo.url_sq_cdn + '" class="frame" onclick="document.location=this.src"><figcaption>' + photo.title + '</figcaption></figure></div>'
+        photoHTML += '<div class="responsive"><div class="gallery"><a target="_blank" href="' + photo.url_sq_cdn + '"><img src="' + photo.url_sq_cdn + '" onerror="imgError(this);" alt="' + photo.title + '" width="600" height="400"></a><div class="desc">' + photo.title + '</div></div></div>'
     })
     $('#photos').html(photoHTML)
+}
+
+function imgError(image){
+    console.log('imgError invoked')
+    image.parentElement.parentElement.style.display = 'none'
 }
 
 function search(event) {
     if (event.key) {
         searchTerm = document.getElementById("myInput").value
-        console.log("search: Enter pressed: searchTerm = ", searchTerm)
-        filterAndDisplayPhotos(photoDataArray)
+        console.log("search: searchTerm = ", searchTerm)
+        filterAndDisplay(photoArray)
     }
 }
