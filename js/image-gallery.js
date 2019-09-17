e0ae2199cb8b273d5d3837c754d106a632b417bd
Dave Hollinden <Dave.Hollinden@ithaka.org>
2019-09-16 20:35:26 -0400

    Add pagination buttons and functionality.

--- js/image-gallery.js
+++ js/image-gallery.js
@@ -2,8 +2,11 @@ $(document).ready(loadPhotoData)
 
 let photoArray = []
 let filteredPhotos = []
-let searchTerm = ""
-let photoHTML = ""
+let searchTerm = ''
+let photoHTML = ''
+let itemsPerPage = 25
+let numPages = ''
+let page = 1
 
 function loadPhotoData() {
     $.getJSON("data.json", function (data) {
@@ -14,7 +17,8 @@ function loadPhotoData() {
 
 function filterAndDisplay(array) {
     filteredPhotos = filterBySearchTerm(array)
-    displayPhotos(filteredPhotos)
+    photoPage = paginatePhotos(filteredPhotos, page)
+    displayPhotos(photoPage)
 }
 
 function filterBySearchTerm(array) {
@@ -22,6 +26,45 @@ function filterBySearchTerm(array) {
         if (searchTerm) return photo.title.toLowerCase().includes(searchTerm.toLowerCase())
         return true // if no search term, return true for all photos
     })
+}
+
+function paginatePhotos(array, page) {
+    numPages = Math.ceil(array.length / itemsPerPage)
+    console.log('paginatePhotos: numPages = ', numPages)
+    page = filteredPhotos.filter(function (photo, i) {
+        return i >= (page - 1) * itemsPerPage && i < page * itemsPerPage
+    })
+    document.getElementById("search-display").innerHTML = "Displaying " + page.length + " of " + array.length  + " images"
+    paginateButtons()
+    return page
+}
+
+function paginateButtons() {
+    if (page === 1) {
+        document.getElementById('prev').className = 'w3-button w3-light-blue w3-small w3-round w3-padding-small w3-disabled'
+        document.getElementById('prev').disabled = true
+        document.getElementById('first').className = 'w3-button w3-light-blue w3-small w3-round w3-padding-small w3-disabled'
+        document.getElementById('first').disabled = true
+    }
+    else {
+        document.getElementById('prev').className = 'w3-button w3-light-blue w3-small w3-round w3-padding-small'
+        document.getElementById('prev').disabled = false
+        document.getElementById('first').className = 'w3-button w3-light-blue w3-small w3-round w3-padding-small'
+        document.getElementById('first').disabled = false
+    }
+    if (page === numPages) {
+        document.getElementById('next').className = 'w3-button w3-light-blue w3-small w3-round w3-padding-small w3-disabled'
+        document.getElementById('next').disabled = true
+        document.getElementById('last').className = 'w3-button w3-light-blue w3-small w3-round w3-padding-small w3-disabled'
+        document.getElementById('last').disabled = true
+    }
+    else {
+        document.getElementById('next').className = 'w3-button w3-light-blue w3-small w3-round w3-padding-small'
+        document.getElementById('next').disabled = false
+        document.getElementById('last').className = 'w3-button w3-light-blue w3-small w3-round w3-padding-small'
+        document.getElementById('last').disabled = false
+    }
+    document.getElementById('pageXOfY').innerHTML = '&nbsp;&nbsp;page ' + page + ' of ' + numPages + '&nbsp;&nbsp;'
 }
 
 function displayPhotos(array) {
@@ -37,14 +80,22 @@ function shortTitle(title) {
 }
 
 function imgError(image){
-    console.log('imgError invoked')
     image.parentElement.parentElement.style.display = 'none'
 }
 
 function search(event) {
     if (event.key) {
         searchTerm = document.getElementById("myInput").value
+        page = 1
         console.log("search: searchTerm = ", searchTerm)
         filterAndDisplay(photoArray)
     }
+}
+
+function toPage(action) {
+    if (action === 'next') page += 1
+    if (action === 'prev') page -= 1
+    if (action === 'first') page = 1
+    if (action === 'last') page = numPages
+    filterAndDisplay(photoArray)
 }
