8820127010c16b7b92d555587e3d4a82a471b4db
Dave Hollinden <Dave.Hollinden@ithaka.org>
2019-09-17 00:00:29 -0400

    Add modal populated with image data. Can save title to photoArray, but not to JSON.

--- js/image-gallery.js
+++ js/image-gallery.js
@@ -70,13 +70,15 @@ function paginateButtons() {
 function displayPhotos(array) {
     photoHTML = ""
     array.forEach(function(photo) {
-        photoHTML += '<div class="responsive"><div class="gallery"><a target="_blank" href="' + photo.url_sq_cdn + '"><img src="' + photo.url_sq_cdn + '" onerror="imgError(this);" alt="' + photo.title + '" width="600" height="400"></a><div class="desc">' + shortTitle(photo.title) + '</div></div></div>'
+        photoHTML += '<div class="responsive"><div class="gallery" onclick="toggleModal(' + photo.id + ')"><img src="' + photo.url_sq_cdn + '" onerror="imgError(this);" alt="' + photo.title + '" width="600" height="400"><div class="desc">' + shortTitle(photo.title) + '</div></div></div>'
     })
     $('#photos').html(photoHTML)
 }
 
 function shortTitle(title) {
-    return title.slice(0, 16) + '...'
+    if (title.length > 16)
+        return title.slice(0, 16) + '...'
+    return title
 }
 
 function imgError(image){
@@ -97,5 +99,50 @@ function toPage(action) {
     if (action === 'prev') page -= 1
     if (action === 'first') page = 1
     if (action === 'last') page = numPages
+    filterAndDisplay(photoArray)
+}
+
+
+// ----- modal -----
+
+function toggleModal(id) {
+    console.log('toggleModal: event = ', id)
+    const modalNode = document.getElementById("myModal")
+    if (modalNode.style.display === 'none' || getComputedStyle(modalNode, null).display === 'none') {
+        modalNode.style.display = 'block'
+        const modalPhoto = photoPage.filter(function(photo) {
+            return parseInt(photo.id) === id
+        })[0]
+        console.log('toggleModal: modalPhoto = ', modalPhoto)
+        document.getElementById('modal-image-title').innerHTML = modalPhoto.title
+        document.getElementById("modal-title-input").value = modalPhoto.title
+        document.getElementById("modal-desc-input").value = modalPhoto.description._content
+        document.getElementById('modal-image-id').innerHTML = 'ID: ' + modalPhoto.id
+        document.getElementById('modal-image-owner').innerHTML = 'Owner Name: ' + modalPhoto.ownername
+        document.getElementById('modal-image-dimensions').innerHTML = 'Image Dimensions: ' + modalPhoto.width_l + ' x ' + modalPhoto.height_l
+        document.getElementById('modal-submit').setAttribute('onclick', 'processForm(' + modalPhoto.id + ')')
+
+    }
+    else
+        modalNode.style.display = 'none'
+}
+
+// if the user clicks anywhere outside of the modal-content, close the modal
+window.onclick = function(event) {
+    const modalNode = document.getElementById("myModal")
+    if (event.target == modalNode) {
+        modalNode.style.display = "none";
+    }
+}
+
+function processForm(id) {
+    console.log('processForm: invoked')
+    console.log('modal-title-input = ', document.getElementById("modal-title-input").value)
+    photoArray.forEach(function(photo) {
+        if (photo.id == id) {
+            photo.title = document.getElementById("modal-title-input").value
+        }
+    })
+    toggleModal()
     filterAndDisplay(photoArray)
 }
